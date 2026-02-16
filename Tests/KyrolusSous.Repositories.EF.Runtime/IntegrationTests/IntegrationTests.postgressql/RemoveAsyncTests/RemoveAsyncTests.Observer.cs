be88defb7c060c7c9d1e433b0bcb74ade8c75f3f
kyroluskamal <kyrolussous@gmail.com>
2026-02-16 17:53:04 +0100

    test(Repositories.EF.Runtime.RemoveRangeAsync): add observer integration coverage

--- Tests/KyrolusSous.Repositories.EF.Runtime/IntegrationTests/IntegrationTests.postgressql/RemoveAsyncTests/RemoveAsyncTests.Observer.cs
+++ Tests/KyrolusSous.Repositories.EF.Runtime/IntegrationTests/IntegrationTests.postgressql/RemoveAsyncTests/RemoveAsyncTests.Observer.cs
@@ -97,6 +97,105 @@ public partial class RemoveAsyncTests
         observer.Reset();
     }
 
+    [Theory(DisplayName = "RemoveRangeAsync emits observer before/after on success")]
+    [MemberData(nameof(ObserverKeyTypeCases))]
+    public async Task RemoveRangeAsync_Observer_OnSuccess(string caseId)
+    {
+        caseId.ShouldNotBeNullOrWhiteSpace();
+        var compositeKey = ObserverKeyTypeSpecs[caseId];
+        using var scope = Factory.Services.CreateScope();
+        var observer = scope.ServiceProvider.GetRequiredService<TestRepositoryObserver>();
+        var uow = scope.ServiceProvider.GetRequiredService<IKyrolusUnitOfWork>();
+        observer.Reset();
+
+        if (compositeKey)
+        {
+            var repo = scope.ServiceProvider.GetRequiredService<KyrolusCompositeKeySoftDeleteRepositoryAsync<ApplicationDbContext, Review>>();
+            var entities = new List<Review>
+            {
+                CreateValidReview(DataSeeder.productBookId, DataSeeder.customerJohnId, rating: 2, comment: "remove-range-observer-composite-1"),
+                CreateValidReview(DataSeeder.productLaptopId, DataSeeder.customerJohnId, rating: 4, comment: "remove-range-observer-composite-2")
+            };
+            await SeedReviewsAsync(entities);
+
+            try
+            {
+                var removed = await repo.RemoveRangeAsync(entities);
+                removed.ShouldBeTrue();
+                (await uow.SaveChangesAsync()).ShouldBeGreaterThan(0);
+
+                var events = observer.Events.Where(e => e.Operation == nameof(repo.RemoveRangeAsync)).ToList();
+                events.Count.ShouldBe(2);
+                events[0].Stage.ShouldBe(ObserverState.Before);
+                events[1].Stage.ShouldBe(ObserverState.After);
+                events[1].Exception.ShouldBeNull();
+            }
+            finally
+            {
+                await CleanupReviewsAsync(entities.Select(x => (x.ProductId, x.CustomerId)));
+                observer.Reset();
+            }
+
+            return;
+        }
+
+        var singleRepo = scope.ServiceProvider.GetRequiredService<KyrolusSingleKeySoftDeleteRepositoryAsync<ApplicationDbContext, Product, Guid>>();
+        var products = new List<Product>
+        {
+            CreateValidProduct(name: "remove-range-observer-single-1"),
+            CreateValidProduct(name: "remove-range-observer-single-2")
+        };
+        await SeedProductsAsync(products);
+
+        try
+        {
+            var removed = await singleRepo.RemoveRangeAsync(products);
+            removed.ShouldBeTrue();
+            (await uow.SaveChangesAsync()).ShouldBeGreaterThan(0);
+
+            var events = observer.Events.Where(e => e.Operation == nameof(singleRepo.RemoveRangeAsync)).ToList();
+            events.Count.ShouldBe(2);
+            events[0].Stage.ShouldBe(ObserverState.Before);
+            events[1].Stage.ShouldBe(ObserverState.After);
+            events[1].Exception.ShouldBeNull();
+        }
+        finally
+        {
+            await CleanupProductsAsync(products.Select(x => x.Id));
+            observer.Reset();
+        }
+    }
+
+    [Theory(DisplayName = "RemoveRangeAsync emits observer after with exception on invalid entities")]
+    [MemberData(nameof(ObserverKeyTypeCases))]
+    public async Task RemoveRangeAsync_Observer_OnFailure(string caseId)
+    {
+        caseId.ShouldNotBeNullOrWhiteSpace();
+        var compositeKey = ObserverKeyTypeSpecs[caseId];
+        using var scope = Factory.Services.CreateScope();
+        var observer = scope.ServiceProvider.GetRequiredService<TestRepositoryObserver>();
+        observer.Reset();
+
+        if (compositeKey)
+        {
+            var repo = scope.ServiceProvider.GetRequiredService<KyrolusCompositeKeySoftDeleteRepositoryAsync<ApplicationDbContext, Review>>();
+            await Should.ThrowAsync<ArgumentNullException>(async () => await repo.RemoveRangeAsync(new Review[] { null! }));
+        }
+        else
+        {
+            var repo = scope.ServiceProvider.GetRequiredService<KyrolusSingleKeySoftDeleteRepositoryAsync<ApplicationDbContext, Product, Guid>>();
+            await Should.ThrowAsync<ArgumentNullException>(async () => await repo.RemoveRangeAsync(new Product[] { null! }));
+        }
+
+        var events = observer.Events.Where(e => e.Operation == "RemoveRangeAsync").ToList();
+        events.Count.ShouldBe(2);
+        events[0].Stage.ShouldBe(ObserverState.Before);
+        events[1].Stage.ShouldBe(ObserverState.After);
+        events[1].Exception.ShouldBeOfType<ArgumentNullException>();
+
+        observer.Reset();
+    }
+
     private static IReadOnlyDictionary<string, bool> BuildObserverKeyTypeSpecs()
         => new Dictionary<string, bool>
         {
