554df5b50839b712400db00dcd6361813cd93209
kyroluskamal <kyrolussous@gmail.com>
2026-08-10 21:59:05 +0200

    fix(validation): fix directory spelling for DataAnnotations.UnitTests and resolve solution association

--- Tests/UnitTests/Validation/KyrolusSous.Validation.DataAnnotations.UnitTests/ValidationDataAnnotationTestsHelper.cs
+++ Tests/UnitTests/Validation/KyrolusSous.Validation.DataAnnotations.UnitTests/ValidationDataAnnotationTestsHelper.cs
@@ -1,14 +1,14 @@
-﻿using Microsoft.VisualStudio.TestPlatform;
+using Microsoft.VisualStudio.TestPlatform;
 
 namespace KyrolusSous.Validation.DataAnnotations.Unitests;
 
 public class ValidationDataAnnotationTestsHelper
 {
-    public static ValueTask<IReadOnlyList<KyrolusValidationFailure>> ValidateAsync(KyrolusValidationFailure request,
+    public static ValueTask<IReadOnlyList<KyrolusValidationFailure>> ValidateAsync(TestUserRequest request,
         KyrolusValidationContext context,
         CancellationToken cancellationToken = default)
     {
         var validator = new DataAnnotationsRequestValidator<TestUserRequest>();
-        return validator.ValidateAsync(null!);
+        return validator.ValidateAsync(request, context, cancellationToken);
     }
 }
